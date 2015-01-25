afe83fe311061e25cdd722f5f7a3eb9977926acf
zhengyang <fubaer@hotmail.com>
2015-01-24 21:48:48 -0800

    build the skeleton for the website

---
+++ resource/javascript/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+/* App Module */
+
+var webApp = angular.module('webApp', ['webAppControllers']);
---
+++ resource/javascript/controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+
+var webAppControllers = angular.module('webAppControllers', []);
+
+webAppControllers.controller('homeCtrl', ['$scope', function($scope){
+	$scope.test = 'lallalallalalal';
+
+}]);
